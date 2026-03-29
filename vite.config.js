eb243762206223f2acf52ac64abb77f78b2d9d31
Andrey <susluk93@mail.ru>
2026-03-29 23:32:41 +0300

    Modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     base: "/",
-    server: {
-        host: '127.0.0.1'
-    },
     optimizeDeps: {
         esbuildOptions: {
         loader: {
