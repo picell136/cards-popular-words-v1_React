f16610e0b1c0c3598ad3096eec2b6aa467ef9576
Andrey <susluk93@mail.ru>
2026-03-30 00:07:04 +0300

    Modified index.html

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/cards-popular-words-v1/",
+    base: "/",
     optimizeDeps: {
         esbuildOptions: {
         loader: {
