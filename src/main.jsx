b71ebfe3abed4eeddd7e560e3c81b56190ac4547
Andrey <susluk93@mail.ru>
2026-04-03 05:55:54 +0300

    Modified main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/cards-popular-words-v1">
+    <BrowserRouter basename="/cards-popular-words-v1_React">
       <App />
     </BrowserRouter>
   </StrictMode>
